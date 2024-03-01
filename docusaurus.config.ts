a7ea992c159865dd2a966ad8ddfd0ff8c0bcc92d
Senih <40578755+senihucar@users.noreply.github.com>
2024-02-29 23:06:36 -0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -113,9 +113,9 @@ const config: Config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'Docusaurus.community',
+        title: 'GraphLab Documentation Node',
         logo: {
-          alt: 'Docusaurus.community Logo',
+          alt: 'Graphic Lab Web Services Logo',
           src: 'img/graphlab_logo.svg',
         },
         hideOnScroll: true,
@@ -185,7 +185,7 @@ const config: Config = {
                 to: 'https://discord.graphlab.ca',
               },
               {
-                label: 'x',
+                label: 'X',
                 to: 'https://x.graphlab.ca',
               },
             ],
@@ -204,7 +204,7 @@ const config: Config = {
             ],
           },
         ],
-        copyright: `<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a><br />Licensed by the Docusaurus community under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.<br />Built with <a href="https://docusaurus.io">Docusaurus v${DOCUSAURUS_VERSION}</a>.<br /><span class="designedBy">Compiled with <svg xmlns="http://www.w3.org/2000/svg" class="heart" width="24" height="24" viewBox="0 0 24 24"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"/></svg>
+        copyright: `Built with <a href="https://docusaurus.io">Docusaurus v${DOCUSAURUS_VERSION}</a>.<br /><span class="designedBy">Compiled with <svg xmlns="http://www.w3.org/2000/svg" class="heart" width="24" height="24" viewBox="0 0 24 24"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"/></svg>
         by <a href="https://senih.dev">senihucar</a></span>.`,
       },
       prism: {
